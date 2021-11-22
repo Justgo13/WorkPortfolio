888675d722bc6fc8cc6a4c1164a2251572fd02e4
justgo13 <jgao2299@gmail.com>
2021-11-22 01:14:03 +0000

    Add react project

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    render() { 
+        return <div></div>;
+    }
+}
+ 
+ReactDom.render(<App/>, document.getElementById("root"));
