246d6e5f128c85fdf674a35d46d9874a39f0b32a
justgo13 <jgao2299@gmail.com>
2021-11-26 16:09:18 -0500

    hide show about me page

--- src/AboutMe/aboutme.js
+++ src/AboutMe/aboutme.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from '../Header/header';
 import Footer from '../Footer/footer';
+import {connect} from 'react-redux';
+import { ABOUT_ME } from '../actions/action_types';
 
 class AboutMe extends Component {
     state = {
@@ -8,14 +10,30 @@ class AboutMe extends Component {
         img_url: "AboutMe.jpg",
         sub_heading: "Carleton University Software Engineering Student"
     }
+
+    
     render() {
-        return (
-            <div>
-                <Header {...this.state}></Header>
-                <Footer></Footer>
-            </div>
-        );
+        if (this.props.active_page === ABOUT_ME) {
+            // active page is About Me, show it
+            return (
+                <div>
+                    <Header {...this.state}></Header>
+                    <Footer></Footer>
+                </div>
+            );
+        } else {
+            // hide page
+            return (
+                <div></div>
+            )
+        }
     }
 }
 
-export default AboutMe;
+const mapStateToProps = (state) => {
+    return {
+        active_page: state.activePageReducer.activePageName.active_page_name
+    }
+}
+
+export default connect(mapStateToProps, null)(AboutMe);
