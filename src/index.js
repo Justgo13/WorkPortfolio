930f062e6e2048e481186c67782e40953fe856d5
justgo13 <jgao2299@gmail.com>
2021-11-25 21:12:17 -0500

    create index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDom from 'react-dom';
+import NavBar from './NavBar/NavBar.js';
+import AboutMe from './AboutMe/aboutme.js';
+import {createStore, applyMiddleware} from "redux";
+import rootReducer from "./reducers/root-reducer";
+import {Provider} from "react-redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from "redux-thunk"
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 
 class App extends React.Component {
     render() { 
-        return <div></div>;
+        return (
+            <Provider store={store}>
+                <div className="container">
+                    <NavBar></NavBar>
+                    <AboutMe></AboutMe>
+                </div>
+            </Provider>
+        );
     }
 }
  
